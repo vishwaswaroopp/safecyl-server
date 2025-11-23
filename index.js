13b84622e72a120b24d932da4984ea658dcf0082
Hydenx2004 <hyden741@gmail.com>
2025-11-23 20:01:09 +0530

    get all data

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "*"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-  })
-);
+
+// Custom CORS middleware to allow all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.use(cors());
 
 // ---- Firebase Admin init (supports 2 styles) ----
 function initFirebase() {
